9c832687246afb12aec3401a2ffd0f6af3fb0ced
hananeltassa <hananeltassa15@gmail.com>
2024-11-03 17:36:56 +0200

    Adding GASP animation

--- js/script.js
+++ js/script.js
@@ -4,3 +4,43 @@ const navLinks = document.getElementById('nav-links');
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+window.addEventListener("load", () => {
+    const heading = document.querySelector(".hero-content h1");
+    const text = heading.innerText;
+    heading.innerHTML = ''; 
+
+    // Split the text 
+    for (let char of text) {
+        const span = document.createElement('span');
+        span.innerText = char; 
+        heading.appendChild(span); 
+    }
+
+    // Animate each letter
+    const chars = heading.querySelectorAll('span');
+    gsap.from(chars, {
+        duration: 0.5,
+        opacity: 0,
+        y: 50,
+        ease: "back.out",
+        stagger: 0.1, 
+        delay: 0.5 
+    });
+    gsap.from(".hero-content p", {
+        duration: 1,
+        opacity: 0,
+        y: 50,
+        ease: "power3.out",
+        delay: 2 
+    });
+
+    gsap.from(".hero-input-group", {
+        duration: 1,
+        opacity: 0,
+        y: 30,
+        ease: "power3.out",
+        delay: 2.3 
+    });
+
+});
